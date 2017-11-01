feb424152f3c9079660b83ddc3f783ed266a0acc
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-01 17:43:26 +0000

    created number and operator array

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ for (var i = 0; i < numberButton.length; i++) {
   var button = numberButton[i]
   button.addEventListener('click', function(even){
     document.getElementById('screen').innerHTML = this.innerHTML
+    chosenNumbers.push(this.innerHTML)
+    console.log(chosenNumbers)
   })
 }
 
 //When number is clicked, store number in array
+var chosenNumbers = []
 
 //Equals sign which will clear top section, join array
 
