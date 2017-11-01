bfd72c8725ff49a3218cd5838fa87f877cda56bf
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-01 17:24:14 +0000

    added some pseudo code

---
+++ main.js
@@ -0,0 +1,10 @@
+//When number is clicked, put into top section
+var button = document.getElementsByClassName('buttonNum')
+button.addEventListener('click',function(event){
+  document.getElementById('screen').innerHTML = button.innerHTML
+})
+//When number is clicked, store number in array
+
+//Equals sign which will clear top section, join array
+
+//AC, clear array and top section
