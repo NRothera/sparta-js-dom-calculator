ee4fefa2a8468560dec5f6b50b1677d5d8dc878f
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-01 19:12:07 +0000

    added event listeners and changed ids for operators

--- main.js
+++ main.js
@@ -3,16 +3,42 @@ var numberButton = document.querySelectorAll('.buttonNum')
 
 for (var i = 0; i < numberButton.length; i++) {
   var button = numberButton[i]
-  button.addEventListener('click', function(even){
+  button.addEventListener('click', function(event){
     document.getElementById('screen').innerHTML = this.innerHTML
-    chosenNumbers.push(this.innerHTML)
-    console.log(chosenNumbers)
+    chosenNumbersAndOperators.push(this.innerHTML)
+    
   })
 }
 
 //When number is clicked, store number in array
-var chosenNumbers = []
 
+//Store operators in array
+
+var plusButton = document.getElementById('plus')
+plusButton.addEventListener('click', function(event){
+  chosenNumbersAndOperators.push('+')
+
+})
+
+var minusButton = document.getElementById('minus')
+plusButton.addEventListener('click', function(event){
+  chosenNumbersAndOperators.push('-')
+
+})
+
+var divideButton = document.getElementById('divide')
+plusButton.addEventListener('click', function(event){
+  chosenNumbersAndOperators.push('/')
+
+})
+
+var multiplyButton = document.getElementById('multiply')
+plusButton.addEventListener('click', function(event){
+  chosenNumbersAndOperators.push('*')
+
+})
+
+var chosenNumbersAndOperators = []
 //Equals sign which will clear top section, join array
 
 //AC, clear array and top section
